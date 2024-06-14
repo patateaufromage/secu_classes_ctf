7ec2a55841b66792b296b565d47f185d7a11a119
bipbip <bip@bip.bip>
2024-06-14 16:52:46 +0200

    esfefes

--- cours/malloc/my_alloc.c
+++ cours/malloc/my_alloc.c
@@ -7,42 +7,44 @@
 #define PAGE_SIZE 4096
 
 
-// Will include a size of type size_t and a flag of type enum chunk_type:
-// From man mmap: 
-	// If addr is NULL, then the kernel chooses the (page-aligned) address at which to create the mapping; this is the most portable method of 
-	// creating a new mapping.  If addr is not NULL, then the kernel takes it as a hint about where to place the mapping;
-struct chunk *heap = NULL;
+struct chunk *meta_heap = NULL;
+struct chunk *data_heap = NULL;
 
 
-// We assign to this variable a size, that we will use with *heap
-// When casting using size_t we convert in int aka in decimal
-size_t heap_size = 4096;
+size_t meat_heap_size = 4096;
+size_t data_heap_size = 4096;
 
 
-// mapping d'une heap de taille predefinie, et non passee en argument de malloc a ce point, de 409 600 000
-struct chunk *init_heap()
+struct chunk *init_meta_heap()
 {
-	if (heap == NULL)
+	if (meta_heap= NULL)
 	{
-		// le mappage de zone sera fait automatiquement etant donne quon a passe NULL en valeur a heap: 
-		// from man mmap: 
-			// For mmap(), offset must be a multiple of the underlying huge page size.  The system automatically aligns length to be a multiple of the underlying huge page size.
-		// de base heap address sera 0x186a0000 = 409 600 000 en decimal 
-		heap = mmap( (void*) (PAGE_SIZE * 100000), heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0 );
+		meta_heap = mmap( (void*) (PAGE_SIZE * 100000), meta_heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0 );
 
-		// on "inscrit" la valeur de la taille dispo, ainsi que le flag indiquant si lespace sera libre ou non, qui seront integres au chunk "ch" de metadata.
-		// la taille a allouer, qui sera indiquee dans le chunk de meta, sera egale a la taille totale creee, moins la taille du chunk contenant les metas:
-		heap->size = heap_size - sizeof(struct chunk);
-		heap->flags = FREE;  // FREE = 0
+		meta_heap->size = meta_heap_size;
+		meta_heap->flags = FREE;  
 	}
 
-	return heap;
+	return meta_heap;
+}
+
+
+struct chunk *init_data_heap()
+{
+	if (data_heap == NULL)
+	{
+		data_heap = mmap( (void*) (PAGE_SIZE * 100000), data_heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0 );
+
+		data_heap->size = heap_size;
+		data_heap->flags = FREE;  // FREE = 0
+	}
+
+	return data_heap;
 }
 
 
 // la valeur retournee sera passee a "last_item" dans get_free_chunk()
 // called using get_free_chunk()
-// determinera la position du dernier chunk et recup son flag 
 struct chunk *get_last_chunk_raw()
 {
 	// item est un pointer et va nous servir a nous ballader dans la heap, et donc on le declare comme egal a la heap en tous points.
@@ -69,7 +71,7 @@ struct chunk *get_last_chunk_raw()
 
 // la valeur retournee sera passee a "item"
 // called using get_free_chunk()
-// this function will identify if a block at the end of the heap is free and of bigger size than the one passed as an argument:
+// this function will identify if a block at the end of the heap is free and of at least the same size than the one passed as an argument:
 struct chunk *get_free_chunk_raw(size_t size)
 {
 	// si la heap n'a pas ete initialisee dans get_free_chunk(), on le fait ici:
@@ -83,13 +85,16 @@ struct chunk *get_free_chunk_raw(size_t size)
 	// on deplace item vers la fin de l'espace de base alloue:
 	for (struct chunk *item = heap; (size_t)item < (size_t)heap + heap_size; item = (struct chunk *) ((size_t)item + sizeof(struct chunk) + item->size))
 	{
-		printf("[+] item reached end of allocated space @ %p\n", (struct chunk *) ((size_t)item + item->size + sizeof(struct chunk)));
+		printf("[+] End of allocated space found @ %p\n", (struct chunk *) ((size_t)item + item->size + sizeof(struct chunk)));
+		printf("[+] ... Checking for free blocks ...\n");
 
 		if (item->flags == FREE && item->size >= size)
 		{
-			printf("[+] Free space found ... assigning it to item !\n");
+			printf("[+] Free block @ %p - bloc flag = %u - writeable bloc size = %lu - size passed = %lu \n", item, item->flags, item->size, size);
 			return item;
 		}
+
+		printf("[+] No free block found @ %p\n", item);
 	}
 	
 	// si on a pas assez d'espace, comme au premier malloc(8192), on return NULL:
@@ -101,15 +106,18 @@ struct chunk *get_free_chunk_raw(size_t size)
 struct chunk *get_free_chunk(size_t size)
 {
 	// if we are at the first use of the program, heap == NULL
-	if (heap == NULL)
+	if (meta_heap == NULL && data_heap == NULL)
 	{
-		heap = init_heap();
-		printf("[!] get_free_chunk() initialised heap @ %p\n", heap);
-		printf("[!] Data size available = %lu - flag = %u\n", heap->size, heap->flags); 
+		meta_heap = init_meta_heap();
+		data_heap = init_data_heap();
+		printf("[!] get_free_chunk() initialised meta_heap && data_heap !!\n");
+		printf("[!] Beggining of meta space @ %p and data space @ %p\n", meta_heap, data_heap);
+		//printf("[!] Chunk size = %lu\n", sizeof(struct chunk));
+		//printf("[!] Data size available = %lu - flag = %u\n", heap->size, heap->flags); 
 	}
 
 	// creation de item, associe au debut de l'espace memoire libre restant.
-	struct chunk *item = get_free_chunk_raw(size); // malloc(8192) retournera NULL
+	struct chunk *meta_ptr = get_free_chunk_raw(size); // malloc(8192) retournera NULL
 
 	if (item == NULL)
 	{
@@ -153,7 +161,7 @@ struct chunk *get_free_chunk(size_t size)
 		// on reassocie item via get free chunk raw ce qui permet de lui redonner la nouvelle addresse calculee, a savoir, item sera place au debut de l'element libre nouvellement cree:
 		printf("[!] Reassigning item new properties ...\n");
 		item = get_free_chunk_raw(size);
-		printf("[!] New space beginning @ %p\n", item);
+		printf("[!] Done, new allocated space begins @ %p\n", item);
 	}
 	return item;
 }
@@ -172,24 +180,28 @@ void *my_alloc(size_t size) {
 	printf("[*] Beginning of my_alloc(%lu)\n", size);
 
 	// creation de ch qui contiendra les metadatas, et pointera vers le debut de l'emplacement libre de taille passee en argument
+	// ch sera associe a item:
 	struct chunk *ch = get_free_chunk(size);
+	printf("[*] Chunk initialised @ %p\n", ch);
 	
-	// ptr pointera vers le debut du chunk libre de taille passee en argument
-	// on cast l'ensemble en void* pour pouvoir output dans le meme type que ptr
+	// ptr pointera a la fin de ch, aka la fin de la partie metadate, et donc au debut de la partie data:
 	ptr = (void*) ((size_t)ch + sizeof(struct chunk));
-	printf("[*] Beginning of data part @ %p\n", ptr);
+	printf("[*] Fin du chunk et debut de l'espace data @ %p\n", ptr);
 	
-	// on associe la fin de la partie allouee a "end":
+	// "end" pointe vers la fin de chunk + data:
 	struct chunk *end = (struct chunk*) ((size_t)ptr + size);
 	end->flags = FREE;
 	end->size = ch->size - sizeof(struct chunk) - size;
-	printf("[*] End of data part @ %p\n", end);
-	printf("[*] Beginning of last data space @ %p\n", (struct chunk *) ((size_t)end + (sizeof(struct chunk))));
+	
+	printf("[*] Data block ends @ %p\n", (struct chunk *) ((size_t)end));
 	printf("[*] Data space available = %lu - flag = %u \n", end->size, end->flags); 
 	
 	ch->flags = BUSY;
 	ch->size = size;	
+
 	printf("[*] my_alloc DONE !\n");
+	printf("[*] ptr @ %p\n", ptr);
+	printf("[*] ...\n");
 	return ptr;
 }
 
