f65011d9153823da165b32ebaf0a761383fda985
bipbip <bip@bip.bip>
2024-06-14 21:25:54 +0200

    esfefes

--- cours/malloc/include/my_alloc.private.h
+++ cours/malloc/include/my_alloc.private.h
@@ -16,10 +16,15 @@ struct chunk
 {
 	size_t size;
 	enum chunk_type flags;
+	//int canary;
+	struct chunk *data_addr;
+	struct chunk *next;
+	struct meta_chunk *prev;
 };
 
 
-struct chunk *init_heap();
+struct chunk *init_meta_heap();
+struct chunk *init_data_heap();
 struct chunk *get_last_chunk_raw();
 struct chunk *get_free_chunk_raw(size_t size);
 struct chunk *get_free_chunk(size_t s);
